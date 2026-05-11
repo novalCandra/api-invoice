fccd362f8a597ec08420cd3c49d41367923dba68
novalCandra <dinatanovel5@gmail.com>
2026-05-11 10:28:42 +0700

    feat : ✅ Add Config and Express Json

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express"
+import dotenv from "dotenv"
+import AuthRouter from "./router/AuthRouter.js";
 const app = express();
 const port = 2000;
 
+dotenv.config();
+app.use(express.json())
+
+app.use("/api", AuthRouter);
 app.get("/", (req, res) => {
     res.send("EXPRESS INVOICE")
 })
