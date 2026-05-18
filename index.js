f3f13283dbe8555265967a5a985b0f9a5b414a1a
novalCandra <dinatanovel5@gmail.com>
2026-05-18 09:44:16 +0700

    Success index

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import AuthRouter from "./router/AuthRouter.js";
 import InvoiceRouter from "./router/InvoiceRouter.js";
-import HistoryRouter from "./router/HistoryRouter.js"
+import HistoryRouter from "./router/HistoryRouter.js";
+import QueueRouter from "./router/QueueRouter.js"
 const app = express();
 const port = 2000;
 
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use("/api", AuthRouter);
 app.use("/api", InvoiceRouter)
 app.use("/api", HistoryRouter)
+app.use("/api", QueueRouter)
 app.get("/", (req, res) => {
     res.send("EXPRESS INVOICE")
 })
