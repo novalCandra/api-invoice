e78440c43a43225e39ed6fa91390bd5d2711d376
novalCandra <dinatanovel5@gmail.com>
2026-05-20 17:49:23 +0700

    feat : ✅ Successful Migration of the Controller to Services (Queue)

--- controller/QueueController.ts
+++ controller/QueueController.ts
@@ -1,32 +1,16 @@
 import prisma from "../config/prisma.js";
 import { Request, Response } from "express";
+import { getAllQueueServices } from "../services/queue.service.js";
 export const allGetQueque = async (req: Request, res: Response) => {
     try {
-        const allQueqe = await prisma.queue.findMany({
-            where: {
-                invoiceId: req.invoice?.id
-            },
-            include: {
-                invoice: {
-                    select: {
-                        id: true, amount: true, clientNama: true, date: true, dueData: true,
-                        histories: true, status: true, queue: true
-                    }
-                }
-            }
+        const invoiceId = Number(req.invoice?.id)
+        const allQueqe = await getAllQueueServices(invoiceId)
+        return res.status(200).json({
+            status: true,
+            message: "Sucesss Create All Queqe",
+            data: allQueqe
         })
-        if (!allQueqe) {
-            return res.status(403).json({
-                status: false,
-                message: "Queue Not GET"
-            })
-        } else {
-            return res.status(200).json({
-                status: true,
-                message: "Sucesss Create All Queqe",
-                data: allQueqe
-            })
-        }
+
     } catch (error) {
         return res.status(500).json({
             status: false,
---
+++ services/queue.service.ts
@@ -0,0 +1,22 @@
+import prisma from "../config/prisma.js"
+export const getAllQueueServices = async (invoiceId: number) => {
+    const allQueqe = await prisma.queue.findMany({
+        where: {
+            invoiceId
+        },
+        include: {
+            invoice: {
+                select: {
+                    id: true, amount: true, clientNama: true, date: true, dueData: true,
+                    histories: true, status: true, queue: true
+                }
+            }
+        }
+    })
+
+    if (!allQueqe) {
+        const error = new Error("not get Queue Data")
+        throw error
+    }
+    return allQueqe;
+}
