6eed0b9cedbee0a7252c70c1207c63b585247614
novalCandra <dinatanovel5@gmail.com>
2026-05-20 17:48:42 +0700

    feat : ✅ Successful Migration of the Controller to Services (History)

--- controller/HistoryController.ts
+++ controller/HistoryController.ts
@@ -1,42 +1,16 @@
-import prisma from "../config/prisma.js";
 import { Request, Response } from "express";
+import { getAllServicesHistory } from "../services/history.service.js";
 export const getAllHistory = async (req: Request, res: Response) => {
     try {
-        const dataHistory = await prisma.history.findMany({
-            where: {
-                userId: req.users?.id,
-                invoiceId: req.invoice?.id
-            },
-            include: {
-                user: {
-                    select: {
-                        id: true,
-                        nama: true,
-                        email: true,
-                        role: true
-                    }
-                },
-                invoices: {
-                    select: {
-                        id: true, amount: true, clientNama: true, date: true, dueData: true,
-                        histories: true, status: true, queue: true
-                    }
-                }
-            }
+        const userId = Number(req.users?.id);
+        const invoiceId = Number(req.invoice?.id)
+        const dataHistory = await getAllServicesHistory(userId, invoiceId)
+        return res.status(200).json({
+            status: true,
+            message: "Success GET",
+            data: dataHistory
         })
-        if (!dataHistory) {
-            return res.status(403).json({
-                status: false,
-                message: "not Gte History"
-            }
-            )
-        } else {
-            return res.status(200).json({
-                status: true,
-                message: "Success GET",
-                data: dataHistory
-            })
-        }
+
     } catch (error) {
         return res.status(500).json({
             status: false,
